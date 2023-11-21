85e8f22e30436706ca8ad891a34f80fb2f49b5d2
leeyujin21 <dkdk506@naver.com>
2023-11-21 12:13:04 +0900

    D1 2047

---
+++ D1/src/Solution2047.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Solution2047 {
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		String T;
+		T=sc.next();
+		T=T.toUpperCase();
+		System.out.println(T);
+	}
+}
